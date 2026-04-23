1db0b72e8bc68e78e46f31fbb6ebce85cd9b9081
Mahdi732 <ey449751@gmail.com>
2026-04-23 16:25:44 +0100

    refactor roundAffection function to use explicit tile values and improve game logic

--- logic/gameLogic.ts
+++ logic/gameLogic.ts
@@ -1,14 +1,70 @@
-import { useGameStore } from "@/store/useGameStore"
+import { Tile, bet, result } from "@/types/game";
 
+export const roundAffection = (bet : bet, userTiles : Tile[], opponentTiles : Tile[] ) : result => {
+    const userHandTile : Tile[] = userTiles;
+    const opponentHandTile : Tile[] = opponentTiles;
 
-export const roundAffection = (bet : string ) => {
-    const userHandValue = useGameStore((state) => state.playerTiles);
-    const opponentHandTile = useGameStore((state) => state.opponentTiles);
+    const userHandValue : number = getTilesValue(userHandTile);
+    const opponentHandValue : number = getTilesValue(opponentHandTile);
 
+    let roundStatus : result = "lose";
+    let winSpecailTail : Tile[] = [];
+    let loseSpecailTail : Tile[] = [];
 
+    if (bet == 'higher') {
+        if (userHandValue > opponentHandValue) {
+            roundStatus = "win";
+            winSpecailTail = getSpecailTail(userHandTile);
+            loseSpecailTail = getSpecailTail(opponentHandTile);
+        } else {
+            winSpecailTail = getSpecailTail(opponentHandTile);
+            loseSpecailTail = getSpecailTail(userHandTile);
+        }
+    } else if (bet == 'lower') {
+        if (userHandValue < opponentHandValue) {
+            roundStatus = "win";
+            winSpecailTail = getSpecailTail(userHandTile);
+            loseSpecailTail = getSpecailTail(opponentHandTile);
+        } else {
+            winSpecailTail = getSpecailTail(opponentHandTile);
+            loseSpecailTail = getSpecailTail(userHandTile);
+        }
+    } else if (bet == 'same') {
+        if (userHandValue == opponentHandValue) {
+            roundStatus = "win";
+            winSpecailTail = getSpecailTail(userHandTile);
+            loseSpecailTail = getSpecailTail(opponentHandTile);
+        } else {
+            winSpecailTail = getSpecailTail(opponentHandTile);
+            loseSpecailTail = getSpecailTail(userHandTile);
+        }
+    }
+
+    createNewHistory(roundStatus, bet, userHandValue, opponentHandValue);
+    editSpecailTailValue(roundStatus, winSpecailTail, loseSpecailTail);
+
+    return roundStatus;
 }
 
 
-const getTilesValue = (tails : []) => {
-    
+const getTilesValue = (tails : Tile[]) => {
+    let value : number = 0;
+
+    tails.forEach((tail) => {
+        value += tail.value;
+    })
+
+    return value;
+}
+
+const getSpecailTail = (tails : Tile[]) : Tile[] => {
+    return tails.filter((tail) => {
+        return tail.type == "dragon" || tail.type == "wind";
+    })
+}
+
+const createNewHistory = (roundStatus : result, betType : bet, userHandValue : number, opponentHandValue : number) => {
+}
+
+const editSpecailTailValue = (roundStatus : result, winSpecailTail : Tile[], loseSpecailTail : Tile[]) => {
 }
