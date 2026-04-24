471b48bb8f99d0e42b77f64768803f82e088f2aa
Mahdi732 <ey449751@gmail.com>
2026-04-24 11:04:11 +0100

    refactor GameState interface and placeBet function for improved clarity and consistency and add edit special tile value

--- store/useGameStore.ts
+++ store/useGameStore.ts
@@ -1,47 +1,66 @@
 import { create } from "zustand";
-import { Tile, RoundHistory, result, bet } from "@/types/game";
+import { Tile, RoundHistory, result, bet, specialTailValue } from "@/types/game";
 import { roundAffection } from "@/logic/gameLogic";
 
 interface GameState {
     score: number;
-    tile : Tile[];
-    discardedTiles : Tile[];
+    tile: Tile[];
+    discardedTiles: Tile[];
     playerTiles: Tile[];
     opponentTiles: Tile[];
     reshuffleCount: number;
     gameHistory: RoundHistory[];
-    specailTailValue : object;
+    specialTailValue: specialTailValue;
     result: result | null;
-    placeBet: (bet: bet, userTiles : Tile[], opponentTiles : Tile[]) => void;
+    placeBet: (bet: bet) => void;
 }
 
 function initialize() {
-    const tilePacket : Tile[] = [];
-    function generateTiles() : Tile[] {return tilePacket}
-    function genrateUserTail() : Tile[] {return []}
-    function genrateOpponentTail() : Tile[] {return []}
+    const tilePacket: Tile[] = [];
+    function generateTiles(): Tile[] { return tilePacket }
+    function genrateUserTail(): Tile[] { return [] }
+    function genrateOpponentTail(): Tile[] { return [] }
     return [generateTiles, genrateUserTail, genrateOpponentTail];
 }
 
 const tileGenerator = initialize();
 
-export const useGameStore = create<GameState>((set) => ({ 
+
+export const useGameStore = create<GameState>((set) => ({
     score: 0,
-    tile : tileGenerator[0](),
-    discardedTiles : [],
+    tile: tileGenerator[0](),
+    discardedTiles: [],
     playerTiles: tileGenerator[1](),
     opponentTiles: tileGenerator[2](),
     reshuffleCount: 0,
     gameHistory: [],
     result: null,
-    specailTailValue : {
-        "dragon" : 5,
-        "wind" : 5,
-        "flower" : 5,
-        "season" : 5,
+    specialTailValue: {
+        dragon: 5,
+        wind: 5,
+        flower: 5,
+        season: 5,
     },
-    placeBet: (bet: bet, userTiles : Tile[], opponentTiles : Tile[]) => {
-        roundAffection(bet, userTiles, opponentTiles);
+    placeBet: (bet: bet) => {
+        set((state) => {
+            const [newSpecialTailValue, newHistory, newScore] = roundAffection(bet, state.playerTiles, state.opponentTiles, state.specialTailValue, state.score);
+
+            const editedDeck = state.tile.map(t => {
+                if (t.type === "number") return t;
+                return {
+                    ...t,
+                    value: newSpecialTailValue[t.type]
+                }
+            })
+
+
+            return {
+                tile: editedDeck,
+                specialTailValue: newSpecialTailValue,
+                gameHistory: [...state.gameHistory, newHistory],
+                score: newScore
+            }
+        })
     }
 
 }))
