5029ec6f69ed91123dcb9754a392a3c9c752b534
Mahdi732 <ey449751@gmail.com>
2026-04-24 11:04:20 +0100

    refactor type definitions for consistency and formatting improvements

--- types/game.ts
+++ types/game.ts
@@ -1,20 +1,27 @@
 export type TileType = "number" | "dragon" | "wind" | "flower" | "season";
 
-export type bet  = "higher" | "lower" | "same";
+export type bet = "higher" | "lower" | "same";
 
-export type result = "win" | "lose" ;
+export type result = "win" | "lose";
 
 export interface Tile {
-    id : string,
-    type : TileType,
-    value : number,
-    name : string
+    id: string,
+    type: TileType,
+    value: number,
+    name: string
 }
 
 export interface RoundHistory {
-    id : string, 
-    bet : bet,
-    playerTileSum : number,
-    opponentTileSum : number,
-    result : result
+    id: string,
+    bet: bet,
+    playerTileSum: number,
+    opponentTileSum: number,
+    result: result
+}
+
+export type specialTailValue = {
+    dragon: number,
+    wind: number,
+    flower: number,
+    season: number,
 }
